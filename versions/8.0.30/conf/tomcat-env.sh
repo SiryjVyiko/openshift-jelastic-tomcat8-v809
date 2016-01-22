7ca40347cb475907b8a421d4f78ba9055134cad3
SiryjVyiko <dz@hivext.net>
2016-01-22 11:34:05 +0200

    Update tomcat-env.sh

--- versions/8.0.30/conf/tomcat-env.sh
+++ versions/8.0.30/conf/tomcat-env.sh
@@ -12,8 +12,7 @@ JASPER_HOME="/opt/repo/versions/8.0.30"
 CATALINA_TMPDIR="/opt/repo/versions/8.0.30/temp"
 CATALINA_BASE="/opt/repo/versions/8.0.30"
 JVM_ID="jelastic"
-JVM_SUFFIX=`hostname | awk -F "." '{ print $6 }'`
-JAVA_OPTS="-Xmn30M -server -Djvm=$JVM_ID -Xms32M -Xmx1501M -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses $JPDA_OPTS $JMX_OPTS -DjvmRid=$JVM_SUFFIX -Dorg.apache.catalina.SESSION_COOKIE_NAME=JELSESSIONID -Dorg.apache.catalina.SESSION_PARAMETER_NAME=jelsessionid"
+JAVA_OPTS="-Xmn30M -server -Djvm=$JVM_ID -Xms32M -Xmx1501M -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses $JPDA_OPTS $JMX_OPTS -Dorg.apache.catalina.SESSION_COOKIE_NAME=JELSESSIONID -Dorg.apache.catalina.SESSION_PARAMETER_NAME=jelsessionid"
 TOMCAT_USER="jelastic"
 let SHUTDOWN_WAIT=2
 
